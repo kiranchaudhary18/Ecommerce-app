3859906f363a096e61fa2afc882ff13b5e43819a
kiranchaudhary18 <kiran.chaudhary.cg@gmail.com>
2025-12-15 21:55:31 +0530

    create the Productitem file and update the latestcollection file code

--- frotend/src/components/LatestCollection.jsx
+++ frotend/src/components/LatestCollection.jsx
@@ -1,21 +1,42 @@
-import React, { useContext } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { ShopContext } from '../context/ShopContext';
 import Title from './Title';
+import ProductItem from './Productitem';
 
 const LatestCollection = () => {
-   
-    const {products} =useContext(ShopContext);
-  
 
-    return (
-        <div className='my-10'>
+  const { products } = useContext(ShopContext);
+  const [latestProducts, setLatestProducts] = useState([]);
 
-            <div className='text-center py-8 text-3xl'>
-                 <Title text1={'LATEST'} text2={'COLLECTION'}/>
-            </div>
+  useEffect(() => {
+    setLatestProducts(products.slice(0, 10));
+  }, [products]);
 
+  return (
+    <div className='my-10'>
+      <div className='text-center py-8 text-3xl'>
+        <Title text1={'LATEST'} text2={'COLLECTION'} />
 
-        </div>
-    );
-}   
+        <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </p>
+      </div>
+
+      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+        {
+          latestProducts.map((item, index) => (
+            <ProductItem
+              key={index}
+              id={item._id}
+              image={item.image}
+              name={item.name}
+              price={item.price}
+            />
+          ))
+        }
+      </div>
+    </div>
+  );
+};
+
 export default LatestCollection;
