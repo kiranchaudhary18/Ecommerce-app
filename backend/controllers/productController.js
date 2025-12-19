797b5382c7044a232a94f8a4a21047f49d901902
kiranchaudhary18 <kiran.chaudhary.cg@gmail.com>
2025-12-19 09:50:27 +0530

    now create backend file and add jwt authentication

---
+++ backend/controllers/productController.js
@@ -0,0 +1,30 @@
+
+
+// function for add product
+
+const addProduct = async (req, res) =>{
+
+
+}
+
+
+// function for add list product
+
+const listProducts = async (req, res)=>{
+
+
+}
+
+//function for removing product
+
+const removeProduct = async (req, res)=>{
+
+
+}
+
+// function for single product
+
+const singleProduct = async (req,res) =>{
+
+}
+
